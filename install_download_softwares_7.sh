6cb35f8455cc4c9306b6662f2fbdcc9412906737
jeremy <chenjm1217@users.noreply.github.com>
2015-05-18 04:03:25 +0800

    fix the python configure param bug, no need ln

--- install_download_softwares_7.sh
+++ install_download_softwares_7.sh
@@ -35,11 +35,11 @@ softwares=("Python-2.7.9"
 # python2.7 must enable share like --enable-share!!! to make YouCompleteMe install ok
 install_steps=( "rm -rf Python-2.7.9:"\
 "tar xvf Python-2.7.9.tar.xz:cd Python-2.7.9:"\
-"./configure --prefix=/usr/local --enable-shared LDFLAGS='-Wl,-rpath /usr/local/lib':"\
+"./configure --prefix=/usr/local --enable-shared LDFLAGS='-Wl,-rpath=/usr/lib64':"\     # 复制一份so到rpath路径下
 "make:sudo make install:"\
-"sudo ln -s /usr/local/lib/libpython2.7.so.1.0  /usr/lib64/libpython2.7.so.1.0"
+#"sudo ln -s /usr/local/lib/libpython2.7.so.1.0  /usr/lib64/libpython2.7.so.1.0"
 # /usr/local/lib/libpython2.7.so.1.0是python安装到/usr/local后的so路径
-# 必须注意要使/usr/lib64/libpython2.7.so.1.0的路径与python查找so的路径匹配。可以通过ldd /dir/to/python来比较
+# 必须注意要使/usr/lib64/libpython2.7.so.1.0的路径与python希望查找的so路径匹配。可以通过ldd /dir/to/python来比较
 
 "rm -rf vim74:"\
 "tar xvf vim-7.4.tar.bz2:cd vim74:"\
