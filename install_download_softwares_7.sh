5590f9cc233430aab90e2ca28c61b458aea78df7
jeremy <chenjm1217@126.com>
2015-05-18 23:05:58 +0800

    add ln libpython2.7.so.1.0

--- install_download_softwares_7.sh
+++ install_download_softwares_7.sh
@@ -34,14 +34,16 @@ softwares=("Python-2.7.9"
 "clang-3.4.2")
 
 # python2.7 must enable share like --enable-share!!! to make YouCompleteMe install ok
-install_steps=( "rm -rf Python-2.7.9:"\
-"tar xvf Python-2.7.9.tar.xz:cd Python-2.7.9:"\
-"./configure --prefix=/usr/local --enable-shared LDFLAGS='-Wl,-rpath=/usr/lib64':"\
-"make:sudo make install:"\
 #"sudo ln -s /usr/local/lib/libpython2.7.so.1.0  /usr/lib64/libpython2.7.so.1.0"
 # -rpath表示复制一份so到该目录下。可用于解决软件非默认安装时找不到相关so的问题
 # /usr/local/lib/libpython2.7.so.1.0是python安装到/usr/local后的so路径
 # 必须注意要使/usr/lib64/libpython2.7.so.1.0的路径与python希望查找的so路径匹配。可以通过ldd /dir/to/python来比较
+
+install_steps=( "rm -rf Python-2.7.9:"\
+"tar xvf Python-2.7.9.tar.xz:cd Python-2.7.9:"\
+"./configure --prefix=/usr/local --enable-shared LDFLAGS='-Wl,-rpath=/usr/lib64':"\
+"make:sudo make install:"\
+"sudo ln -s /usr/local/lib/libpython2.7.so.1.0  /usr/lib64/libpython2.7.so.1.0"
 
 "rm -rf vim74:"\
 "tar xvf vim-7.4.tar.bz2:cd vim74:"\
